1e7bbd4e98f6e56003cdce9a237225264e2c7b60
Cassia <11emcassia@gmail.com>
2026-08-07 09:30:34 +0800

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# Digital Gallery Site - Cassia
-Cassia's Repo for the Digital Gallery Site
+# Digital Gallery Walk Site
+A site for displaying select artworks from 11-EM-Cassia (2026-2027), serving as a digital and immortalized version of the "Gallery Walk" project.
+
+## Features
+**I. Smooth Animations**
+- A unique and interactive landing page animation
+- Website element animations
+
+**II. Viewable/Downloadable Artworks**
+- Appropriately and aptly displays the different chosen artworks
+- Zoom in/out function on each of the artworks
+- Ability to download the selected artwork
+
+**III. Credits/Role Display**
+- Efficiently displays the roles/contributions of each participant of the Gallery Walk.
+
+**IV. Miscellaneous**
+- Responsive Design (Allows for compatibility with typical smartphones and laptops)
+- Header with quick links to each of the different sections of the website
+- Footer with copyright and more quick links (social medias, relevant links)
+
+## Reason for Development
+Our "Gallery Walk" project for Finite Mathematics is only displayed physically, and is only unfortunately displayed for a maximum of 2 weeks. Via this website, the artworks can be displayed for an indefinite amount of time, for future eyes to see.
+
+## How to Use the Program
+1. Access the website's page on ***[insert deployed link here]***. No need to install anything on your local device.
+2. Explore the site and view beautiful artworks.
+3. Choose to download any artworks you find beautiful and worthy to keep on your device.
+4. View the credits to see the roles and contributions of each member of the Gallery Walk.
+
+## Screenshots!
+***[To be Added]***
---
+++ js/app.js
@@ -0,0 +1,78 @@
+/*
+The Digital Gallery Walk Site is a website which can display several downloadable artworks based on previously-set categories. 
+Copyright (C) 2026 11-Cassia
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
+//LANDING
+
+const viewArtButton = document.querySelector("#LANDING-PAGE-BUTTON");
+const landing = document.querySelectorAll(".landing");
+const landvid = document.querySelector("#WEBSITE-LANDING-CONT")
+
+window.addEventListener("load", () => {
+    const introText = document.querySelector("#LANDING-PAGE-INTRO");
+    introText.classList.add("hide");
+
+    introText.addEventListener("animationend", (anim) => {
+        if (anim.animationName === "fade") {
+            introText.style.visibility = "hidden";
+            introText.style.opacity = "0"
+        }
+    })
+})
+
+viewArtButton.addEventListener("click", async () => {
+    landing.forEach(el => {
+        const anim = el.animate(
+            [
+                { transform: "translateY(0)", opacity: "1"},
+                { transform: "translateY(-120vh)", opacity: "0" }
+            ],
+            {
+                duration: 1000,
+                easing: "ease",
+                fill: "forwards"
+            }
+        );
+
+        anim.onfinish = () => {
+            el.remove();
+        };
+    });
+
+    landvid.classList.remove("active");
+});
+
+// ZOOM IN
+const card = document.querySelectorAll(".artwork-card");
+const zoomCont = document.querySelector("#ZOOMED-IN-CONTAINER");
+const zoomContBtn = document.querySelector("#ZOOM-CONT-CLOSE");
+
+card.forEach((el) => {
+    el.addEventListener("click", () => {
+        const download = document.querySelector("#ZOOM-CONT-DOWNLOAD");
+        const imgHolder = document.querySelector("#ZOOM-IMG");
+        zoomCont.classList.add("active");
+        const img = el.firstElementChild.src;
+
+        imgHolder.src = img;
+        download.href = String(img)
+    })
+});
+
+zoomContBtn.addEventListener("click", () => {
+    zoomCont.classList.remove("active")
+})
